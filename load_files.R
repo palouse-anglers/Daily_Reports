54f8542e2aae5bce62aa49ceccc3cdc7d6123cd0
Steve Hummel <palouseanglers@gmail.com>
2024-04-23 20:51:41 -0700

    updated load files

--- load_files.R
+++ load_files.R
@@ -1,11 +1,153 @@
+# load other peoples functions
+
 library("XML")
 library("xml2")
 library("tidyverse")
 library("highcharter")
+library("googlesheets4")
+library("googledrive")
 
 
-srv_path <- "../../Downloads/Steve/Steve/P4_Files_SRV/"
-dmm_path <- "../../Downloads/Steve/Steve/P4_Files_DMM/"
+options(googledrive_quiet=TRUE)
+
+# paths -------------------------------------------------------------------
+
+# spreadsheets ------------------------------------------------------------
+
+# download_2023_spreadsheet <- 
+#   drive_get(as_id("15M1zH7LF1ceNxNezROKcTCgcCfqlW7yY")) %>% 
+#   drive_download(path="C:/Users/humme/Documents/Daily_Reports/2023/spreadsheet/tagging_2023.xlsx",overwrite = TRUE)
+
+lgr_daily_names <- c("Elastomer HCH1",
+                     "HCH1", "WCH1",
+                     "HCH0", "WCH0",
+                     "HSTHD","WSTHD",	
+                     "HSOC",
+                     "WSOC",	
+                     "COHO")
+
+download_2024_spreadsheet <- 
+  drive_get(as_id("1_RpuJnZFD86aYpLJrP7QOL2ezCjx4Svs")) %>% 
+  drive_download(
+    path="C:/Users/humme/Documents/Daily_Reports/2024/spreadsheet/tagging_2024.xlsx",overwrite = TRUE)
+
+
+# --------------WDFW Transport: Table Row = Handled/transported---------
+wdfw_trans_2024 <- 
+  readxl::read_xlsx("C:/Users/humme/Documents/Daily_Reports/2024/spreadsheet/tagging_2024.xlsx",
+                    sheet = "WDFW tran report totals",skip = 2) %>%
+  select(1,13:22)%>%
+  janitor::clean_names(case = "screaming_snake") %>%
+  mutate(TAG_DATE=update(as.Date(DATE),year=2024),.before="DATE")%>%
+  select(-DATE) %>%
+  set_names(c("TAG_DATE",lgr_daily_names)) %>%
+  mutate(Type="WDFD Trans",
+         Table="Handled/transported")  
+
+# -------------------Transport------------------------------------------
+transport_2024 <- 
+  readxl::read_xlsx("C:/Users/humme/Documents/Daily_Reports/2024/spreadsheet/tagging_2024.xlsx",
+                    sheet = "Transport tagged",skip = 3) %>%
+  janitor::clean_names(case = "screaming_snake") %>%
+  mutate(TAG_DATE=update(lubridate::as_date(as.numeric(TAG_DATE)),year=2024))%>%
+  mutate(across(c(HC:TAGGED),~as.numeric(.x)),
+         across(c(HC:TAGGED),~replace_na(.x,0)),
+         TAG_DATE= TAG_DATE- day(1)
+  ) %>%
+  mutate(Type="Transport",Table="Tagged/transported")%>%
+  select(TAG_DATE,ELAST_FC,HSFC,WSFC,HC,WC,HS,WS,W_SOCK,H_SOCK,COHO,Type,Table)
+
+#------------------------ Survival---------------------------------------
+survival_2024 <- 
+  readxl::read_xlsx("C:/Users/humme/Documents/Daily_Reports/2024/spreadsheet/tagging_2024.xlsx",
+                    sheet = "Survival tagged",skip = 3) %>%
+  janitor::clean_names(case = "screaming_snake") %>%
+  mutate(TAG_DATE=update(lubridate::as_date(as.numeric(TAG_DATE)),year=2024))%>%
+  mutate(across(c(HC:TAGGED),~as.numeric(.x)),
+         across(c(HC:TAGGED),~replace_na(.x,0)),
+         TAG_DATE= TAG_DATE- day(1)
+) %>%
+  mutate(Type="Survival",Table="Tagged/bypassed") %>%
+  select(TAG_DATE,ELAST_FC,HSFC,WSFC,HC,WC,HS,WS,W_SOCK,H_SOCK,COHO,Type,Table)
+
+
+
+# Bycatch
+Bycatch_2024 <- 
+  readxl::read_xlsx("C:/Users/humme/Documents/Daily_Reports/2024/spreadsheet/tagging_2024.xlsx",
+                    sheet = "Bycatch",skip = 3) %>%
+  janitor::clean_names(case = "screaming_snake") %>%
+  mutate(TAG_DATE=update(lubridate::as_date(as.numeric(TAG_DATE)),year=2024)) %>%
+  mutate(across(c(CATFISH:TOTALS),~as.numeric(.x)),
+         across(c(CATFISH:TOTALS),~replace_na(.x,0))
+         ) %>%
+  mutate(Type="Bycatch",Table=NA_character_)
+
+
+
+table_row_names <- 
+c("Tagged/bypassed",
+"Tagged/transported",
+"Handled/bypass",
+"Handled/transported",
+"Examined (GBT Only)",
+"Handled/not examined (GBT only)",
+"Sacrificed",
+"Morts")
+
+
+# survuval and transport
+daily_spreadsheet <- 
+  survival_2024 %>%
+    bind_rows(transport_2024)
+
+# wdfw trans
+wdfw_trans_2024
+ 
+
+# xml files ---------------------------------------------------------------
+
+get_xml_from_drive <- function(year){
+
+  
+drive_paths <- 
+  googledrive::drive_ls(path = "https://drive.google.com/drive/folders/1HCAvBYxgUqPsQa6A-QnJiQoRrpv--b_p")
+  
+# table of directories
+xml_files <- drive_paths %>%
+  filter(name=={{year}}) %>%
+  googledrive::drive_ls() %>%
+  filter(name %in% c("P4 Files","P4_Data")) %>%
+  googledrive::drive_ls(recursive = TRUE,pattern = ".*xml")  
+# table of ids
+# xml_ids <- 
+#   map_dfr(xml_files$id,
+#   ~drive_ls(as_id(.x))) %>%
+#   select(name,id) %>%
+#   drive_reveal(., "created_time")
+
+  walk2(xml_files$id,xml_files$name,
+       ~drive_get(as_id(.x)) %>%
+       drive_download(overwrite = TRUE,
+       path = glue::glue("C:/Users/humme/Documents/Daily_Reports/{year}/xml_files/{.y}")
+       )
+   )
+}
+
+
+
+
+# Save xml files ----------------------------------------------------------
+
+# get_xml_from_drive("2023")
+
+get_xml_from_drive("2024")
+
+# paths to files ----------------------------------------------------------
+
+
+# srv_path <- "../../Downloads/Steve/Steve/P4_Files_SRV/"
+# dmm_path <- "../../Downloads/Steve/Steve/P4_Files_DMM/"
 
 # read_xml ----------------------------------------------------------------
 
@@ -18,124 +160,117 @@ xml_to_dataframe <- function(file_path,...){
   
   # loop through paths and read xml files
   # combine rowwise 
-  map_dfr(srv_paths,
+  df <- map_dfr(srv_paths,
           ~ xmlParse(.x) %>%
-            xmlToDataFrame(nodes = getNodeSet(., "//MRREvent"))) %>%
+            xmlToDataFrame(nodes = getNodeSet(., "//MRREvent")) %>%
+            mutate(Type=case_when(
+                   str_detect(.x,"SRV") & str_detect(.x,"PRE") ~ "Pre-Survival",
+                   str_detect(.x,"SRV") & str_detect(.x,"SV") ~ "Survival",
+                   str_detect(.x,"MRT") ~ "Mortality",
+                   str_detect(.x,"GAA") ~ "Gordon Axel",
+                   str_detect(.x,"ILR") ~"Lemhi",
+                   TRUE ~ "Transport/Unknown"),
+                   path=basename(.x)
+                   
+          )) %>%
     # clean date
     mutate(Date_Time=lubridate::ymd_hms(EventDate),
            Date=as.Date(Date_Time))
   
+cli::cli_alert_info("Dates from {.val {range(df$Date)[1]}} to {.val {range(df$Date)[2]}}")
+
+return(df)  
+  
+  }
+
+
+
+
+
+
+
+# Test
+# xmljune_16 <- 
+#   xml_to_dataframe(file_path = glue::glue("{getwd()}/2023/xml_files/")) 
+# 
+# 
+# april_9_2024 <- 
+#   xml_to_dataframe(file_path = glue::glue("{getwd()}/2024/xml_files/"))
+
+
+april_2024 <-
+  xml_to_dataframe(file_path = glue::glue("{getwd()}/2024/xml_files/"))
+
+
+
+# pre survival tag check --------------------------------------------------
+
+
+
+pre_survival_check <- function(data){
+   
+pre_survival <- 
+  data %>%
+  filter(Type %in% "Pre-Survival") %>%
+  pull(PITTag)
+
+any(data$PITTag[!data$Type == "Pre-Survival"] %in% pre_survival)
+
 }
 
 
-# apply the function to load everything into a single dataframe 
-srv_df <- xml_to_dataframe(srv_path)
-dmm_df <- xml_to_dataframe(dmm_path)
-
-
-
-
-# t=srv_df %>%
-#   mutate(Species=case_when(
-#     SpeciesRunRearType=="32H" ~ "HSTHD",
-#     SpeciesRunRearType=="32W" ~ "WSTHD",
-#     TRUE ~ "Other"
-#     )
-#   ) %>%
-#   group_by(Date,Species) %>%
-#   add_tally(name = "Daily_Count") %>%
-#   # mutate(across(c(Weight,Length,MarkTemperature),
-#   #               ~mean(as.numeric(.x),na.rm = TRUE),
-#   #               .names = "mean_{.col}")
-#   #        ) %>%
-#   arrange(Date) %>%
-#   pivot_wider(names_from = "Species",
-#               values_from = c("Daily_Count","mean_MarkTemperature")) %>%
-#   ungroup()
-# 
-# nrow(t)
-
-
-# t2 <- t %>% 
-#   #filter(Date=="2023-06-02") %>%
-#   #distinct(Date,HSTHD,WSTHD) %>%
-#   group_by(Date) %>%
-#   mutate(across(c(HSTHD,WSTHD),~sum(.x,na.rm = TRUE)),
-#             Daily_Total= HSTHD+WSTHD
-#             ) %>%
-#   distinct(Date,HSTHD,WSTHD,.keep_all = TRUE)
-#   
+# Check any pre tags slipped through
+pre_survival_check(april_2024)
   
-
-
-# srv_df %>%
-#   mutate(Weight= as.numeric(Weight),
-#          Length= round(as.numeric(Length),2),
-#          Species=case_when(
-#            SpeciesRunRearType=="32H" ~ "Hatchery",
-#            SpeciesRunRearType=="32W" ~ "Wild",
-#            TRUE ~ "Other")
-#   ) %>%
-#   hchart(., "column", hcaes(x = Date, y = Length, group = Species)) %>%
-#   hc_tooltip(formatter = JS("function(){
-#                             return ('Tag:' + this.point.PITTag + 
-#                             ' <br>Date: ' + this.point.Date +
-#                              ' <br>Length: ' + this.point.Length
-#                             )
-#                             }"))
-
-
-
-
-
-# srv_df %>%
-#   filter(Date=="2023-06-02") %>%
-#   mutate(Species=case_when(
-#     SpeciesRunRearType=="32H" ~ "HSTHD",
-#     SpeciesRunRearType=="32W" ~ "WSTHD",
-#     TRUE ~ "Other")
-#   ) %>%
-#   group_by(Date,Species) %>%
-#   add_tally(name = "Daily_Count") %>%
-#   arrange(Date) %>%
-#   pivot_wider(names_from = "Species",
-#               values_from = "Daily_Count") %>%
-#   ungroup() %>%
-#   group_by(Date) %>%
-#   mutate(across(c(HSTHD,WSTHD),~sum(.x,na.rm = TRUE)),
-#          Daily_Total= HSTHD+WSTHD
-#   ) %>%
-#   distinct(Date,HSTHD,WSTHD,.keep_all = TRUE) %>%
-#  
-
 # Item plot ---------------------------------------------------------------
 
+code_lookup <- read_csv("code_lookup.csv")
+
+make_xml_summary <- function(data){
   
-  srv_df_sum <- srv_df  %>%
-    mutate(Species=case_when(
-    SpeciesRunRearType=="32H" ~ "Hatchery",
-    SpeciesRunRearType=="32W" ~ "Wild",
-    TRUE ~ "Other")
-  ) %>%
-  group_by(Date,Species) %>%
+  data %>%
+  left_join(code_lookup,by=c("SpeciesRunRearType"="Code")) %>% 
+  rename(Species=Description) %>%
+  group_by(Type,Date,Species) %>%
   add_tally(name = "Daily_Count") %>%
   arrange(Date) %>%
-  distinct(Date,Species,Daily_Count) %>%
+  distinct(Date,Species,Daily_Count,.keep_all=TRUE) %>%
   ungroup() %>%
   group_by(Date) %>%
   mutate(Daily_Total=sum(Daily_Count))%>%
-  ungroup() 
+  ungroup()
+}
 
-srv_df_sum_wide <- srv_df_sum%>%
+
+
+
+# April 10th 2024 ---------------------------------------------------------
+
+ april_2024 %>%
+   filter(Date %in% '2024-04-23') %>%
+         make_xml_summary() %>%
+  select(Type,Date,Species,Daily_Count,WDFW)
+
+ daily_spreadsheet %>%
+   filter(TAG_DATE %in% '2024-04-23')
+ 
+ wdfw_trans_2024 %>%
+   filter(TAG_DATE %in% '2024-04-23')
+ 
+ 
+ 
+ april_10_2024 %>%
+   make_xml_summary() %>%
+   filter(Date %in% "2024-04-10") %>%
   pivot_wider(names_from = "Species",
               values_from = "Daily_Count") 
 
 
 ddm_df_sum <- dmm_df   %>%
   mutate(Species=case_when(
-    SpeciesRunRearType=="32H" ~ "Hatchery",
-    SpeciesRunRearType=="32W" ~ "Wild",
-    TRUE ~ "Other")
+    SpeciesRunRearType=="32H" ~ "32Hatchery",
+    SpeciesRunRearType=="32W" ~ "32Wild",
+    TRUE ~ SpeciesRunRearType)
   ) %>%
   group_by(Date,Species) %>%
   add_tally(name = "Daily_Count") %>%
@@ -145,6 +280,8 @@ ddm_df_sum <- dmm_df   %>%
   group_by(Date) %>%
   mutate(Daily_Total=sum(Daily_Count))%>%
   ungroup() 
+
+
 # srv_df_sum %>%
 # group_by(Date) %>%
 # nest() %>%
